df0384f310b0896444127fd4eabb078b203a2b7e
Deepesh Soni <31923438+magnifer@users.noreply.github.com>
2025-08-17 22:34:44 +0530

    Update 04.SortArray.cpp

--- DSA/450/01.Arrays/04.SortArray.cpp
+++ DSA/450/01.Arrays/04.SortArray.cpp
@@ -4,9 +4,11 @@
  * Created on:   2025-08-06
  * Description:  Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
  * Problem Link:
- *
+ * Space Complexity: O(1)   
+ * Time Complexity:  O(n) Dutch national Flag problem  
+ * 
  * Compiler:     MSVC 19.41, C++17
- * Build:        cl /std:c++17 /EHsc MaxAndMinElementInArray.cpp
+ * Build:        cl /std:c++17 /EHsc Sort.cpp
  *
  * License:      MIT License (or specify your license)
  ************************************************************/
@@ -17,23 +19,45 @@
 
 using namespace std;
 
-int KthSmallestElement(vector<int> &arr, int &k)
+vector<int> Sort(vector<int> &arr)
 {
     int n = arr.size();
-
-    for (int i = 0; i < n; ++i)
+    int currrentIndex = 0;
+    int onesStartIndex = 0;
+    int twosStartIndex = n - 1;
+    while (currrentIndex <= twosStartIndex)
     {
-        sort(arr.begin(), arr.end());
+        if (arr[currrentIndex] == 0)
+        {
+            std::swap(arr[currrentIndex], arr[onesStartIndex]);
+            onesStartIndex++;
+            currrentIndex++;
+        }
+        else if (arr[currrentIndex] == 2)
+        {
+            std::swap(arr[currrentIndex], arr[twosStartIndex]);
+            twosStartIndex--;
+        }
+        else
+        {
+            currrentIndex++;
+        }
     }
 
-    return arr[k - 1];
+    return arr;
 }
 
 int main()
 {
-    vector<int> arr{-1, 2, -3, 4, 5, 6};
-    int k = 2;
-    cout << KthSmallestElement(arr, k);
+    vector<int> arr{0, 1, 1, 0, 0, 2, 1, 0, 2};
+    Sort(arr);
+    for (auto element : arr)
+        cout << element << " ";
 
     return 0;
 }
+
+/*
+* Alternate way:
+* std::sort() complexity O(nlogn)
+*/
